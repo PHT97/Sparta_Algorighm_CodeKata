17e07384afe4a334dec2d5140a025d1eb876a06e
PHT97 <park9704271@naver.com>
2024-01-11 10:06:58 +0900

    5.숫자 비교하기

---
+++ 5.숫자 비교하기/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _5.숫자_비교하기
+{
+    internal class Program
+    {
+        public static int solution(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(2,3));
+            Console.WriteLine(solution(11, 11));
+            Console.WriteLine(solution(7, 99));
+        }
+    }
+}
