83821d7c37d4a6938d196b6c58827bd96d9d957d
PHT97 <park9704271@naver.com>
2024-01-11 10:00:39 +0900

    3.몫 구하기

---
+++ 3.몫 구하기/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _3.몫_구하기
+{
+    internal class Program
+    {
+        public static int solution(int num1, int num2)
+        {
+            int answer = 0;
+            answer = num1 / num2;
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(10,5));
+            Console.WriteLine(solution(7,2));
+        }
+    }
+}
