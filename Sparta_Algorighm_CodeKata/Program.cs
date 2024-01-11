7e6b166b6eb11be226b27777e0963cf1e871683e
PHT97 <park9704271@naver.com>
2024-01-11 09:18:21 +0900

    1일차. 두수의차

--- Sparta_Algorighm_CodeKata/Program.cs
+++ Sparta_Algorighm_CodeKata/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        public static int soulution(int num1, int num2)
+        {
+            int answer = 0;
+            answer = num1 - num2;
+            return answer;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(soulution(2, 3));
         }
     }
 }
