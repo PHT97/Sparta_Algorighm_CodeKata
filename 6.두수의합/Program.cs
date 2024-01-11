d3e31f234ff65afb5731889c1e6c81dc127651b3
PHT97 <park9704271@naver.com>
2024-01-11 10:09:07 +0900

    6.두수의 합

---
+++ 6.두수의합/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _6.두수의합
+{
+    internal class Program
+    {
+        public static int solution(int num1, int num2)
+        {
+            int answer = 0;
+            answer = num1 + num2;
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(2,3));
+            Console.WriteLine(solution(100, 2));
+        }
+    }
+}
