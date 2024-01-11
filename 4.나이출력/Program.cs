ddbba38f7e126c406977f6faf6981ffe048be936
PHT97 <park9704271@naver.com>
2024-01-11 10:03:09 +0900

    4.나이출력

---
+++ 4.나이출력/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _4.나이출력
+{
+    internal class Program
+    {
+        public static int solution(int age)
+        {
+            int answer = 0;
+            answer = 2022 - age + 1;
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(40));
+            Console.WriteLine(solution(23));
+        }
+    }
+}
