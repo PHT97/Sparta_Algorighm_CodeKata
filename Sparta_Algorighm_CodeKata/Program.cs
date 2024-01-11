8369e0fedb1b140fa2043708fdccad818300dd7c
PHT97 <park9704271@naver.com>
2024-01-11 09:02:58 +0900

    초기 커밋.

---
+++ Sparta_Algorighm_CodeKata/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sparta_Algorighm_CodeKata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
