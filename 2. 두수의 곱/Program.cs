beca3752c56b1591de9aaa826d11ee594dce7c9a
PHT97 <park9704271@naver.com>
2024-01-11 09:57:59 +0900

    2.두수의곱

---
+++ 2. 두수의 곱/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2._두수의_곱
+{
+    internal class Program
+    {
+        public static int solution(int num1, int num2)
+        {
+            int answer = 0;
+            answer = num1 * num2;
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(3, 4));
+            Console.WriteLine(solution(27, 19));
+        }
+    }
+}
