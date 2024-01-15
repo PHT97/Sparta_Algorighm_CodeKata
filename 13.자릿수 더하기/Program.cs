8c9bec3170b4eca0ea6f2d0c15ae8995772e42f5
PHT97 <park9704271@naver.com>
2024-01-15 09:53:37 +0900

    13

---
+++ 13.자릿수 더하기/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _13.자릿수_더하기
+{
+    internal class Program
+    {
+
+        public static int solution(int n)
+        {
+            int answer = 0;
+            while (n > 0)
+            {
+                answer += n % 10;
+                n = n / 10;
+            }
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(123));
+            Console.WriteLine(solution(987));
+        }
+    }
+}
