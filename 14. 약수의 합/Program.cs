85a267f3d703e6dbe4a16004b42b9fd600778677
PHT97 <park9704271@naver.com>
2024-01-16 09:42:55 +0900

    14.15

---
+++ 14. 약수의 합/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+namespace _14._약수의_합
+{
+    internal class Program
+    {
+        public static int solution(int n)
+        {
+            int answer = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    answer += i;
+                }
+            }
+            return answer + n;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(12));
+            Console.WriteLine(solution(5));
+        }
+    }
+}
---
+++ 15.나머지가 1이되는 수 찾기/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _15.나머지가_1이되는_수_찾기
+{
+    internal class Program
+    {
+        public static int solution(int n)
+        {
+            int answer = 0;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 1)
+                {
+                    answer = i;
+                    break;
+                }
+            }
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(10));
+            Console.WriteLine(solution(12));
+        }
+    }
+}
