c7ee7cb24ecc9c7e0f2a0ca34b2fed211d540cd6
PHT97 <park9704271@naver.com>
2024-01-17 09:58:19 +0900

    16

---
+++ 16. x만큼 간격이 있는 n개의 숫자/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _16._x만큼_간격이_있는_n개의_숫자
+{
+    internal class Program
+    {
+        public static long[] solution(int x, int n)
+        {
+            long[] answer = new long[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i != 0)
+                    answer[i] = answer[i - 1] + x;
+                else
+                    answer[i] = x;
+            }
+            return answer;
+        }
+    }
+}
