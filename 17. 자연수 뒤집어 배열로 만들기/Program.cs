5bb5af712c1c382b823bc3a17b2e5c84f9824253
PHT97 <park9704271@naver.com>
2024-01-18 09:50:05 +0900

    17

---
+++ 17. 자연수 뒤집어 배열로 만들기/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _17._자연수_뒤집어_배열로_만들기
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int[] solution(long n)
+            {
+                string a = n.ToString();
+                int[] answer = new int[a.Length];
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    answer[i] = (int)(n % 10);
+                    n = n / 10;
+                }
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
