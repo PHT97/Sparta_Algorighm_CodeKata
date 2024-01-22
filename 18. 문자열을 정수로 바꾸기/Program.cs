9f123eaed3805b2d0d513db3bd309ecbc47ab79d
PHT97 <park9704271@naver.com>
2024-01-22 09:34:33 +0900

    18

---
+++ 18. 문자열을 정수로 바꾸기/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _18._문자열을_정수로_바꾸기
+{
+    internal class Program
+    {
+        public static int solution(string s)
+        {
+            int answer = 0;
+            answer = int.Parse(s);
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution("1234"));
+        }
+    }
+}
