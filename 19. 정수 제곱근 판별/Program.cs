0dc5cd7069a9b3b421cbe71439565edaf0cf0ff7
PHT97 <park9704271@naver.com>
2024-01-23 09:37:47 +0900

    19

---
+++ 19. 정수 제곱근 판별/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _19._정수_제곱근_판별
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public long solution(long n)
+            {
+                long answer = 0;
+                for (long i = 1; i <= n; i++)
+                {
+                    if (i * i == n)
+                    {
+                        answer = (i + 1) * (i + 1);
+                        break;
+                    }
+                    else if (i * i >= n)
+                    {
+                        break;
+                    }
+                }
+                if (answer == 0)
+                {
+                    answer = -1;
+                }
+                return answer;
+            }
+        }
+    }
+}
