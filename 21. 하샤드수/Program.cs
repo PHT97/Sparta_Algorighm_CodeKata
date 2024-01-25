29e2b0eea083c43d435f64d6ffc97e12fbaa0808
PHT97 <park9704271@naver.com>
2024-01-25 09:27:06 +0900

    21

---
+++ 21. 하샤드수/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _21._하샤드수
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public bool solution(int x)
+            {
+                bool answer = false;
+                int originX = x;
+                int sum = 0;
+                while (x > 0)
+                {
+                    sum += x % 10;
+                    x = x / 10;
+                }
+                if (originX % sum == 0)
+                {
+                    answer = true;
+                }
+                return answer;
+            }
+        }
+    }
+}
