d4e5e5711febc3757a9bf9e5a001ebdfb7d5e6cf
PHT97 <park9704271@naver.com>
2024-01-29 09:52:28 +0900

    22

---
+++ 22. 두 정수 사이의 합/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _22._두_정수_사이의_합
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public long solution(int a, int b)
+            {
+                long answer = 0;
+                if (a < b)
+                {
+                    for (int i = a; i <= b; i++)
+                    {
+                        answer += i;
+                    }
+                }
+                else if (a > b)
+                {
+                    for (int i = b; i <= a; i++)
+                    {
+                        answer += i;
+                    }
+                }
+                else
+                {
+                    answer = a;
+                }
+                return answer;
+            }
+        }
+    }
+}
