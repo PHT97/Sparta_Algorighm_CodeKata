55b02b7ea9e1e13bfc75075b2d213698488db2f9
PHT97 <park9704271@naver.com>
2024-02-02 09:41:58 +0900

    24

---
+++ 24. 서울에서 김서방 찾기/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _24._서울에서_김서방_찾기
+{
+    public class Solution
+    {
+        public string solution(string[] seoul)
+        {
+            string answer = "";
+            for (int i = 0; i < seoul.Length; i++)
+            {
+                if (seoul[i] == "Kim")
+                {
+                    answer = "김서방은 " + i + "에 있다";
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
