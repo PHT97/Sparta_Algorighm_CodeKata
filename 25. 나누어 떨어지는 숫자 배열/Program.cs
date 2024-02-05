5d840642b47140aeddc3c16c9ac8e426f9dff0be
PHT97 <park9704271@naver.com>
2024-02-05 09:47:24 +0900

    25

---
+++ 25. 나누어 떨어지는 숫자 배열/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] arr, int divisor)
+    {
+        List<int> answerList = new List<int>();
+
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            if (arr[i] % divisor == 0)
+            {
+                answerList.Add(arr[i]);
+            }
+        }
+
+        if (answerList.Count == 0)
+        {
+            answerList.Add(-1);
+        }
+        else
+        {
+            // 오름차순 정렬
+            answerList.Sort();
+        }
+
+        return answerList.ToArray();
+    }
+}
