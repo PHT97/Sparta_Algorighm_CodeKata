de2a9bf03aa8f5c78ab3b558c8ed4d74b5710960
PHT97 <park9704271@naver.com>
2024-02-19 09:34:57 +0900

    29

---
+++ 29. 제일 작은수 제거하기/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+public class Solution
+{
+    public int[] solution(int[] arr)
+    {
+        List<int> answer = arr.ToList();
+
+        int min = answer.Min();
+        answer.Remove(min);
+
+        if (answer.Count < 1)
+            answer.Add(-1);
+
+        return answer.ToArray();
+    }
+}
