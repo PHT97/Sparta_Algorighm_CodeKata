919faafd59e36494330a796f1d9cd685bc18a063
PHT97 <park9704271@naver.com>
2024-03-13 09:53:04 +0900

    41

---
+++ 41. 이상한 문자 만들기/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public string solution(string s)
+    {
+        string answer = "";
+        int num = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+            {
+                answer += s[i];
+                num = 0;
+                continue;
+            }
+            if (num % 2 == 0)
+            {
+                //대문자로 변경
+                answer += s[i].ToString().ToUpper();
+                num++;
+            }
+            else
+            {
+                //소문자로 변경
+                answer += s[i].ToString().ToLower();
+                num++;
+            }
+        }
+
+        return answer;
+    }
+}
