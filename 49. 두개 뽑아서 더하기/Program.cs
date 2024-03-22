be36da4239b7752925e50ffed08d8b8b14afcb94
PHT97 <park9704271@naver.com>
2024-03-22 19:22:48 +0900

    49

---
+++ 49. 두개 뽑아서 더하기/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] numbers)
+    {
+        int[] answer = new int[] { };
+
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                list.Add(numbers[i] + numbers[j]);
+            }
+        }
+        //list 오름차순 Sort() 정렬
+        list.Sort();
+
+        //Distinct().ToArray() 중복 삭제
+        answer = list.Distinct().ToArray();
+
+        return answer;
+    }
+}
