faaa40789cbc725a375a29c85c2802ca212bf1fb
PHT97 <park9704271@naver.com>
2024-03-29 10:00:17 +0900

    53

---
+++ 53. 명예의전당(1)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(int k, int[] score)
+    {
+        int[] answer = new int[score.Length];
+        int[] list = new int[k];
+        int target;
+        int min;
+        int temp;
+
+        for (int i = 0; i < list.Length; i++)
+        {
+            list[i] = -1;
+        }
+
+        for (int i = 0; i < score.Length; i++)
+        {
+            target = score[i];
+            min = 9999;
+            for (int j = 0; j < list.Length; j++)
+            {
+                if (list[j] < target)
+                {
+                    temp = list[j];
+                    list[j] = target;
+                    target = temp;
+                }
+
+                if (list[j] > -1 && min > list[j])
+                {
+                    min = list[j];
+                }
+            }
+            answer[i] = min;
+        }
+
+        return answer;
+    }
+}
